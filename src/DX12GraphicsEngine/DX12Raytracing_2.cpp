40b2f46090aca6287b27ce6fb6b7f5746fc327d0
BillFlugrath <bflugrath@sbcglobal.net>
2025-11-04 22:24:37 -0800

    Update DX12Raytracing_2.cpp
    Add comments.

--- src/DX12GraphicsEngine/DX12Raytracing_2.cpp
+++ src/DX12GraphicsEngine/DX12Raytracing_2.cpp
@@ -78,12 +78,14 @@ void DX12Raytracing_2::OnInit()
 	//descriptor_heap_srv_ = new DXDescriptorHeap();
 	//descriptor_heap_srv_->Initialize(static_cast<ComPtr<ID3D12Device>>(m_dxrDevice), D3D12_DESCRIPTOR_HEAP_TYPE_CBV_SRV_UAV, D3D12_DESCRIPTOR_HEAP_FLAG_SHADER_VISIBLE, kMaxNumOfCbSrvDescriptorsInHeap);
 
+	//load assets from disk and create D3D resources
 	LoadModelsAndTextures();
 
+	//Set hit group for TLAS objects
 	m_pD3DModel_0->SetHitGroupIndex(0);
 	m_pD3DModel_1->SetHitGroupIndex(1);
 
-	//These are for Bound textures
+	//Create D3DTexture for later processing
 	D3DTexture tex0,tex1,tex2;
 	tex0.m_pTextureResource = m_pDXTexture_0->GetDX12Resource();
 	tex1.m_pTextureResource = m_pDXTexture_1->GetDX12Resource();
@@ -91,17 +93,18 @@ void DX12Raytracing_2::OnInit()
 
 	std::vector< D3DTexture > vTextures{ tex0, tex1, tex2 }; //bound textures
 
-	//m_pD3DSceneTextures2D holds unbound textures
+	//m_pD3DSceneTextures2D holds unbound textures of type textures2D
 	m_pD3DSceneTextures2D->AddTexture(tex0); //index 0
 	m_pD3DSceneTextures2D->AddTexture(tex1);  //index 1
 
 
-	//create constant buffers
+	//get texture width and height.  Currently setting these into HLSL to do texture.Load.
+	//TODO just use texture samplers.  Remove the texture loads.
 	int width = 0, height = 0, width1 = 0, height1 = 0;
 	m_pDXTexture_0->GetWidthAndHeight(width, height);
 	m_pDXTexture_1->GetWidthAndHeight(width1, height1);
 
-	//create models with vb amd ib
+	//create D3DMeshes with vb amd ib
 	D3DMesh mesh0, mesh1, mesh2, mesh3;
 	DXGraphicsUtilities::CreateD3DMesh(m_pDXMesh_0, &mesh0); //cube
 	DXGraphicsUtilities::CreateD3DMesh(m_pDXMesh_1, &mesh1); //teapot
@@ -109,7 +112,7 @@ void DX12Raytracing_2::OnInit()
 	DXGraphicsUtilities::CreateD3DMesh(m_pDXMesh_3, &mesh3); //axes
 
 	
-	//Create D3DModel objects that contain mesh objects
+	//Add mesh objects to the D3DModel objects.  The meshes have the actual ib and vb resources.
 	m_pD3DModel_0->AddMesh(mesh0);
 	m_pD3DModel_0->AddMesh(mesh3);
 	//m_pD3DModel_0->AddMesh(mesh3);
@@ -117,19 +120,19 @@ void DX12Raytracing_2::OnInit()
 	//m_pD3DModel_1 has 1 mesh
 	m_pD3DModel_1->AddMesh(mesh2);
 
-	//SetTexture2DIndex sets the same index for all meshes in model
+	// SetTexture2DIndex sets the same index for all meshes in model
 	m_pD3DModel_0->SetTexture2DIndex(0);
 	m_pD3DModel_1->SetTexture2DIndex(1);
 
+	// SetTexture2DIndexForMesh allow sfor setting the texture for a specific mesh in the model
 	m_pD3DModel_0->SetTexture2DIndexForMesh(0, 1); //set mesh0 texture1
-
 
 
 	//Create the Final Scene by adding models to a vector.  Each model becomes a  TLAS instance
 	m_pD3DSceneModels->AddModel(*m_pD3DModel_0);
 	m_pD3DSceneModels->AddModel(*m_pD3DModel_1);
 	
-	
+	//Create BLAS and TLAS
 	m_pDXRManager->CreateTopAndBottomLevelAS(*m_pD3DSceneModels); 
 
 	if (m_bUseBoundResources == true)
@@ -143,7 +146,7 @@ void DX12Raytracing_2::OnInit()
 	
 
 	//create constant buffer D3D12 resources. The CBV (view descriptors) are made after in CreateCBVSRVUAVDescriptorHeap
-	m_pDXRManager->CreateConstantBufferResources((float)width);
+	m_pDXRManager->CreateConstantBufferResources((float)width); //Used for texture.LOAD.  TODO just use sampler.
 	m_pDXRManager->CreateCBVSRVUAVDescriptorHeap(*m_pD3DSceneModels, vTextures);  //Sets BOUND textures and bound models
 
 	if (m_bUseBoundResources==false)
@@ -159,11 +162,13 @@ void DX12Raytracing_2::OnInit()
 
 void DX12Raytracing_2::LoadModelsAndTextures()
 {
+	//load obj models into meshes
 	m_pDXMesh_0->LoadModelFromFile("./assets/models/cube.obj", static_cast<ID3D12Device*>(m_dxrDevice));
 	m_pDXMesh_1->LoadModelFromFile("./assets/models/unitTeapot.obj", static_cast<ID3D12Device*>(m_dxrDevice));
 	m_pDXMesh_2->LoadModelFromFile("./assets/models/unitsphere.obj", static_cast<ID3D12Device*>(m_dxrDevice));
 	m_pDXMesh_3->LoadModelFromFile("./assets/models/axes.obj", static_cast<ID3D12Device*>(m_dxrDevice));
 
+	//load 2d textures
 	wstring texture_filepath = kTestPNGFile;
 	ComPtr< ID3D12Device> pDevice(m_dxrDevice);
 	ComPtr< ID3D12CommandQueue> pCommandQueue(m_CmdQueue);
@@ -180,7 +185,6 @@ void DX12Raytracing_2::LoadModelsAndTextures()
 	assert(bLoaded && "Failed to load texture!");
 
 	
-
 	//load cube map
 	ID3D12Device* device = static_cast<ID3D12Device*>(m_dxrDevice);
 	ID3D12GraphicsCommandList* cmdList= static_cast<ID3D12GraphicsCommandList*>(m_dxrCommandList);
@@ -189,12 +193,16 @@ void DX12Raytracing_2::LoadModelsAndTextures()
 	m_pDDSCubeMap_0->CreateDDSTextureFromFile12(device, pCommandQueue, szFileName);
 }
 
-// Update frame-based values.
+// Update frame-based values and update CB data for use by shaders.
 void DX12Raytracing_2::OnUpdate()
 {
 	m_pDXCamera->Update();
 	m_pDXRManager->Update(m_pDXCamera->GetViewMatrix(), m_pDXCamera->GetPosition(), m_pDXCamera->GetFOV());
-	//m_pDXRManager->UpdateUnboundCB(*m_pD3DSceneModels, *m_pD3DSceneTextures2D);
+
+	if (m_bUseBoundResources == false)
+	{
+		m_pDXRManager->UpdateUnboundCB(*m_pD3DSceneModels, *m_pD3DSceneTextures2D);
+	}
 }
 
 void DX12Raytracing_2::OnRender()
